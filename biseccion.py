1299112bfad876fab6c34329c49c0620bed06baf
Khris <khristianhfs06@gmail.com>
2024-01-22 04:41:38 -0400

    Subir primera version del codigo de biseccion

---
+++ biseccion.py
@@ -0,0 +1,33 @@
+import math
+
+def biseccion(f,a,b,E,Ni):
+
+    Ea = 1 #error aproximado
+    i = 1 #contador del numero de iteraciones
+    m_actual = 0 #Punto medio actual
+    m_anterior = 0 #Punto medio anterior
+
+    while i < Ni  and Ea > E:
+        m_anterior = m_actual
+        m_actual = (a + b)/2
+        if f(m_actual) * f(b) < 0:
+            a = m_actual
+        else:
+            b = m_actual
+
+        if i >= 1:
+            Ea = abs((m_actual - m_anterior) / m_actual)
+
+        i += 1
+
+    print(m_actual)
+    print(Ea)
+    print(i)
+
+def f(x):
+    return (x-2)**2 - math.log(x)
+a = 1
+b = 2
+E = 0.04
+Ni = 5
+biseccion(f, a, b, E, Ni)
